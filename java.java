3be6c0519b3ea903c6da11d84ed310ef1b3e2032
Mahalakshmi Sudheshna Garre <113832187+Maha2646@users.noreply.github.com>
2024-10-06 14:31:12 +0530

    Create java.java

---
+++ java.java
@@ -0,0 +1,82 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountHashPartitioner {
+
+    // Mapper Class
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    // Partitioner Class using Hash Function
+    public static class HashPartitioner extends Partitioner<Text, IntWritable> {
+
+        @Override
+        public int getPartition(Text key, IntWritable value, int numPartitions) {
+            // Use a hash function to partition the keys
+            return Math.abs(key.hashCode()) % numPartitions;
+        }
+    }
+
+    // Reducer Class
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count with hash partitioner");
+
+        job.setJarByClass(WordCountHashPartitioner.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setPartitionerClass(HashPartitioner.class);  // Set custom hash-based partitioner
+        job.setReducerClass(IntSumReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // Set the number of reducers, should match with the expected number of partitions
+        job.setNumReduceTasks(3);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
+
